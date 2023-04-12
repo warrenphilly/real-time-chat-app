b176d1a07ca2f11c6fa623d7d5ca8f17f25ad0cf
Warren Phillips <38818772+warrenphilly@users.noreply.github.com>
2023-04-12 10:31:39 -0400

    first commit

--- server.js
+++ server.js
@@ -67,5 +67,5 @@ io.on('connection', (socket) => {
 const PORT = process.env.PORT || 3000;
 //listen to port
 server.listen(PORT, () => {
-    console.log("Server is running on port ${PORT}");
+    console.log(`Server is running on port ${PORT}`);
 });
